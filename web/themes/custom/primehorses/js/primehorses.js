484ffeb62ee37abfb961bdf82952c87c4b802f6b
Yunior Cabrera <yuniorcabrerahdez@gmail.com>
2017-06-08 14:58:07 -0500

    fixed load arrow despues del ajax y subiendo configuracion

---
+++ web/themes/custom/primehorses/js/primehorses.js
@@ -0,0 +1,18 @@
+/**
+ * Created by yunior on 6/6/2017.
+ */
+
+(function ($, Drupal) {
+
+    Drupal.behaviors.colorboxSlick = {
+        attach: function (context, settings) {
+           /** $(context).find('#slick-node-125-gallery-full-1-slider').once('myCustomBehavior').each(function () {
+                //Apply the myCustomBehaviour effect to the elements only once.
+            }); */
+
+            jQuery.colorbox.settings.onComplete = function(){
+                $(document).find('#slick-node-125-gallery-full-1-slider').slick();
+            };
+        }
+    };
+})(jQuery, Drupal);
